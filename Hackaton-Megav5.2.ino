159ea75c94be497f6332953342483b6c9d10ce42
Leonardo Robotics Club Vratsa <milentsolov64@gmail.com>
2016-05-19 16:01:01 +0300

    Update Hackaton-Megav5.2.ino

--- Hackaton-Megav5.2.ino
+++ Hackaton-Megav5.2.ino
@@ -1,16 +1,18 @@
-//#include <Servo.h>
-#include <ColorLCDShield.h>
+//#include <SparkFunColorLCDShield.h>
+
+#include <Servo.h>
+
 #include <Time.h>
 //#include <TimeLib.h>
-#include <SPI.h>
 #include <MFRC522.h>
+#include <SPI.h>
 #include <Keypad.h>
 #include <LiquidCrystal_I2C.h>
 #include <Wire.h>
-LCDShield clcd;
+//LCDShield clcd;
 #define RST_PIN         49     
 #define SS_PIN          53
-//Servo myservo;
+Servo myservo;
 
 String passIvo = "12346A";
 String lockIvo = "12346B";
@@ -37,317 +39,387 @@ const byte COLS = 4;
 const byte ROWS = 4;
 
 char keys [ROWS][COLS] = {
-  {'1', '2', '3', 'A'},
-  {'4', '5', '6', 'B'},
-  {'7', '8', '9', 'C'},
-  {'*', '0', '#', 'D'}
+  {
+    '1', '2', '3', 'A'  }
+  ,
+  {
+    '4', '5', '6', 'B'  }
+  ,
+  {
+    '7', '8', '9', 'C'  }
+  ,
+  {
+    '*', '0', '#', 'D'  }
 };
 
 
-byte rPins[ROWS] = {30, 32, 34, 36};
-byte cPins[COLS] = {31, 33, 35, 37};
+byte rPins[ROWS] = {
+  30, 32, 34, 36};
+byte cPins[COLS] = {
+  31, 33, 35, 37};
 
 Keypad kl = Keypad( makeKeymap(keys), rPins, cPins, ROWS, COLS);
-
+float t;
 void setup() {
-    Serial.begin(9600);
-    while (!Serial);
-    SPI.begin();
-    mfrc522.PCD_Init();
-  clcd.init(EPSON);
-  clcd.contrast(43);
-  clcd.clear(WHITE);
-    pinMode(A14, OUTPUT);
-    //pinMode(A15,  OUTPUT);
+  setTime(00, 00, 00, 6, 2, 2016);
+  t=second();
+  Serial.begin(9600);
+  while (!Serial);
+  SPI.begin();
+  mfrc522.PCD_Init();
+  //clcd.init(EPSON);
+  //clcd.contrast(43);
+  //clcd.clear(WHITE);
+  //pinMode(A14, OUTPUT);
+  //pinMode(A15,  OUTPUT);
 
-analogWrite(A15,0);
-//analogWrite(A15,150);
+  //analogWrite(A15,0);
+  //analogWrite(A15,150);
 
-    lcd.begin(16,2);
-    Wire.begin(); 
+  lcd.begin(16,2);
+  Wire.begin(); 
 
-    //myservo.attach(28);
+
 }
 
 
 
 void dump_byte_array(byte *buffer, byte bufferSize) {
-    readRfid="";
-    for (byte i = 0; i < bufferSize; i++) {
-        readRfid=readRfid + String(buffer[i], HEX);
-    }
+  readRfid="";
+  for (byte i = 0; i < bufferSize; i++) {
+    readRfid=readRfid + String(buffer[i], HEX);
+  }
+
+  myservo.attach(44);
 }
 void loop() {
+
+
+  //xtime();
   lcd.setBacklight(HIGH);
-    
+  myservo.attach(44);
 
   ident();
-   analogWrite(A15,0);
-  
-  
+  //analogWrite(A15,0);
+
+
   if ( ! mfrc522.PICC_IsNewCardPresent())
-        return;
+    return;
   if ( ! mfrc522.PICC_ReadCardSerial())
-        return;
+    return;
 
   dump_byte_array(mfrc522.uid.uidByte, mfrc522.uid.size);
 
-  
-for(int i = 0; i < 6; i){
-  char pKey = kl.getKey();
-  if (pKey != NO_KEY){
-    i++;
-    pass = pass + pKey;
-    Serial.println(pass);
-    
-  }
-  
-  //clcd.clear(WHITE);
-  if(readRfid == okRfid_1) {
-    lcd.setCursor(0,0);    
-    lcd.print("Hello, Ivo!");
+
+  for(int i = 0; i < 6; i){
+    char pKey = kl.getKey();
+    if (pKey != NO_KEY){
+      i++;
+      pass = pass + pKey;
+      Serial.println(pass);
+
+    }
+
     //clcd.clear(WHITE);
-        lcd.setCursor(0,1);
-     lcd.print("             ");
-    clcd.setStr("Hello, Ivo!           ", 2,2,RED,WHITE);
+    if(readRfid == okRfid_1) {
+      lcd.setCursor(0,0);    
+      lcd.print("Hello, Ivo!");
+      //clcd.clear(WHITE);
+      lcd.setCursor(0,1);
+      lcd.print("             ");
+      //clcd.setStr("Hello, Ivo!           ", 2,2,RED,WHITE);
+    }
+
+    if(readRfid == okRfid_2) {
+      lcd.setCursor(0,0);
+
+      lcd.print("Hello, Milen!");
+      lcd.setCursor(0,1);
+      lcd.print("             ");
+      //clcd.clear(WHITE);
+      //clcd.setStr("Hello, Milen!         ", 2,2,RED,WHITE);
+    }
+
+    if(readRfid == okRfid_3) {
+      lcd.setCursor(0,0);
+
+      lcd.print("Hello, Teo!");
+      lcd.setCursor(0,1);
+      lcd.print("             ");
+      //clcd.clear(WHITE);
+      //clcd.setStr("Hello, Teo!         ", 2,2,RED,WHITE);
+    }
   }
 
-  if(readRfid == okRfid_2) {
-    lcd.setCursor(0,0);
-    
-    lcd.print("Hello, Milen!");
-        lcd.setCursor(0,1);
-        lcd.print("             ");
-    //clcd.clear(WHITE);
-    clcd.setStr("Hello, Milen!         ", 2,2,RED,WHITE);
-  }
-  
-  if(readRfid == okRfid_3) {
-    lcd.setCursor(0,0);
-    
-    lcd.print("Hello, Teo!");
-        lcd.setCursor(0,1);
-        lcd.print("             ");
-    //clcd.clear(WHITE);
-    clcd.setStr("Hello, Teo!         ", 2,2,RED,WHITE);
-  }
-}
 
 
 
-  
   if (readRfid==okRfid_1 && pass == passIvo) {
-    analogWrite(A15, 200);
+    //analogWrite(A15, 200);
+    //lcd.clear();
+    //lcd.setCursor(0,0);
+    lcd.print("Unlocked");
+    //clcd.clear(WHITE);
+    //clcd.setStr("Unlocked", 2,2,RED,WHITE);
+    //myservo.attach(44);
+    myservo.write(120);
+    //myservo.detach();
+
+    delay(2000);
+    //clcd.clear(WHITE);
     lcd.clear();
-    lcd.setCursor(0,0);
-      lcd.print("Unlocked");
-      clcd.clear(WHITE);
-      clcd.setStr("Unlocked", 2,2,RED,WHITE);
-      //myservo.write(0);
-      
-      delay(2000);
-      clcd.clear(WHITE);
-      lcd.clear();
-          
-      }
+
+  }
 
   if (readRfid==okRfid_1 && pass == lockIvo) {
-   analogWrite(A15,100); 
-   lcd.clear();
-    clcd.clear(WHITE);
-    clcd.setStr("Locked", 2,2,RED,WHITE);
+    //analogWrite(A15,100); 
+    lcd.clear();
+    //clcd.clear(WHITE);
+    //clcd.setStr("Locked", 2,2,RED,WHITE);
     lcd.setCursor(0,0);
-      lcd.print("Locked");
-     // myservo.write(120);
-     
-      delay(2000);
-      lcd.clear();
-      clcd.clear(WHITE);
-      
-      }
+    lcd.print("Locked");
+    //myservo.attach(44);
+    myservo.write(0);
+    //myservo.detach();
+
+    delay(2000);
+    lcd.clear();
+    //clcd.clear(WHITE);
+
+  }
 
 
   if (readRfid != okRfid_1 && (pass == passIvo || pass == lockIvo) && pass != "") {
     lcd.clear();
-    clcd.clear(WHITE);
+    //clcd.clear(WHITE);
     lcd.setCursor(0,0);
-      lcd.print("Don`t match");
-      clcd.setStr("Don't match", 2,2,RED,WHITE);
-      delay(1000);
-      lcd.clear();
-          
-      }
+    lcd.print("Don`t match");
+    //clcd.setStr("Don't match", 2,2,RED,WHITE);
+    delay(1000);
+    lcd.clear();
+
+  }
 
 
   if (readRfid==okRfid_1) {
     if(pass != lockIvo){
       if(pass != passIvo){
         lcd.clear();
-        clcd.clear(WHITE);
-    lcd.setCursor(0,0);
-      lcd.print("Don`t Match");
-      clcd.setStr("Don`t Match", 2,2,RED,WHITE);
-      delay(1000);
-      clcd.clear(WHITE);
-      lcd.clear();
+        //clcd.clear(WHITE);
+        lcd.setCursor(0,0);
+        lcd.print("Don`t Match");
+        //clcd.setStr("Don`t Match", 2,2,RED,WHITE);
+        delay(1000);
+        //clcd.clear(WHITE);
+        lcd.clear();
       }
     }
-    
-    
-          
-      }
+
+
+
+  }
 
 
 
 
 
-        if (readRfid==okRfid_3 && pass == passTeo) {
-    analogWrite(A15, 200);
+  if (readRfid==okRfid_3 && pass == passTeo) {
+    //analogWrite(A15, 200);
     lcd.clear();
     lcd.setCursor(0,0);
-      lcd.print("Unlocked");
-      clcd.clear(WHITE);
-      clcd.setStr("Unlocked", 2,2,RED,WHITE);
-      //myservo.write(0);
-      
-      delay(2000);
-      clcd.clear(WHITE);
-      lcd.clear();
-          
-      }
+    lcd.print("Unlocked");
+    //clcd.clear(WHITE);
+    //clcd.setStr("Unlocked", 2,2,RED,WHITE);
+
+    //myservo.attach(44);
+    myservo.write(120);
+    //myservo.detach();
+
+    delay(2000);
+    //clcd.clear(WHITE);
+    lcd.clear();
+
+  }
 
   if (readRfid==okRfid_3 && pass == lockTeo) {
-   analogWrite(A15,100); 
-   lcd.clear();
-    clcd.clear(WHITE);
-    clcd.setStr("Locked", 2,2,RED,WHITE);
+    //analogWrite(A15,100); 
+    lcd.clear();
+    //clcd.clear(WHITE);
+    //clcd.setStr("Locked", 2,2,RED,WHITE);
     lcd.setCursor(0,0);
-      lcd.print("Locked");
-     // myservo.write(120);
-     
-      delay(2000);
-      lcd.clear();
-      clcd.clear(WHITE);
-      
-      }
+    lcd.print("Locked");
+    //myservo.attach(44);
+    myservo.write(0);
+    //myservo.detach();
+
+    delay(2000);
+    lcd.clear();
+    //clcd.clear(WHITE);
+
+  }
 
 
   if (readRfid != okRfid_3 && (pass == passTeo || pass == lockTeo) && pass != "") {
     lcd.clear();
-    clcd.clear(WHITE);
+    //clcd.clear(WHITE);
     lcd.setCursor(0,0);
-      lcd.print("Don`t match");
-      clcd.setStr("Don't match", 2,2,RED,WHITE);
-      delay(1000);
-      lcd.clear();
-          
-      }
+    lcd.print("Don`t match");
+    //clcd.setStr("Don't match", 2,2,RED,WHITE);
+    delay(1000);
+    lcd.clear();
+
+  }
 
 
   if (readRfid==okRfid_3) {
     if(pass != lockTeo){
       if(pass != passTeo){
         lcd.clear();
-        clcd.clear(WHITE);
-    lcd.setCursor(0,0);
-      lcd.print("Don`t Match");
-      clcd.setStr("Don`t Match", 2,2,RED,WHITE);
-      delay(1000);
-      clcd.clear(WHITE);
-      lcd.clear();
+        //clcd.clear(WHITE);
+        lcd.setCursor(0,0);
+        lcd.print("Don`t Match");
+        //clcd.setStr("Don`t Match", 2,2,RED,WHITE);
+        delay(1000);
+        //clcd.clear(WHITE);
+        lcd.clear();
       }
     }
-    
-    
-          
-      }
-      
-      
-      
-      
-      
-      
-        if (readRfid==okRfid_2 && pass == passMilen) {
-    analogWrite(A15, 200);
+
+
+
+  }
+
+
+
+
+
+
+  if (readRfid==okRfid_2 && pass == passMilen) {
+    //analogWrite(A15, 200);
     lcd.clear();
     lcd.setCursor(0,0);
-      lcd.print("Unlocked");
-      clcd.clear(WHITE);
-      clcd.setStr("Unlocked", 2,2,RED,WHITE);
-      //myservo.write(0);
-      
-      delay(2000);
-      clcd.clear(WHITE);
-      lcd.clear();
-          
-      }
+    lcd.print("Unlocked");
+    //clcd.clear(WHITE);
+    //clcd.setStr("Unlocked", 2,2,RED,WHITE);
+    //myservo.attach(44);
+    myservo.write(120);
+    //myservo.detach();
+
+    delay(2000);
+    //clcd.clear(WHITE);
+    lcd.clear();
+
+  }
 
   if (readRfid==okRfid_2 && pass == lockMilen) {
-   analogWrite(A15,100); 
-   lcd.clear();
-    clcd.clear(WHITE);
-    clcd.setStr("Locked", 2,2,RED,WHITE);
+    //analogWrite(A15,100); 
+    lcd.clear();
+    //clcd.clear(WHITE);
+    //clcd.setStr("Locked", 2,2,RED,WHITE);
     lcd.setCursor(0,0);
-      lcd.print("Locked");
-     // myservo.write(120);
-     
-      delay(2000);
-      lcd.clear();
-      clcd.clear(WHITE);
-      
-      }
+    lcd.print("Locked");
+
+    myservo.write(0);
+    //myservo.detach();
+
+    delay(2000);
+    lcd.clear();
+    //clcd.clear(WHITE);
+
+  }
 
 
   if (readRfid != okRfid_2 && (pass == passMilen || pass == lockMilen) && pass != "") {
     lcd.clear();
-    clcd.clear(WHITE);
+    //clcd.clear(WHITE);
     lcd.setCursor(0,0);
-      lcd.print("Don`t match");
-      clcd.setStr("Don't match", 2,2,RED,WHITE);
-      delay(1000);
-      lcd.clear();
-          
-      }
+    lcd.print("Don`t match");
+    //clcd.setStr("Don't match", 2,2,RED,WHITE);
+    delay(1000);
+    lcd.clear();
+
+  }
 
 
   if (readRfid==okRfid_2) {
     if(pass != lockMilen){
       if(pass != passMilen){
         lcd.clear();
-        clcd.clear(WHITE);
-    lcd.setCursor(0,0);
-      lcd.print("Don`t Match");
-      clcd.setStr("Don`t Match", 2,2,RED,WHITE);
-      delay(1000);
-      clcd.clear(WHITE);
-      lcd.clear();
+        //clcd.clear(WHITE);
+        lcd.setCursor(0,0);
+        lcd.print("Don`t Match");
+        //clcd.setStr("Don`t Match", 2,2,RED,WHITE);
+        delay(1000);
+        //clcd.clear(WHITE);
+        lcd.clear();
       }
     }
-    
-    
-          
-      }
-     
+
+
+
+  }
+
   pass = "";
   lcd.clear();
-  clcd.clear(WHITE);
+  //clcd.clear(WHITE);
 
-  
-      
+
+
 }
 
 
 void ident(){
-  clcd.setStr("Security System", 2,6,RED,WHITE);
-   clcd.setStr("Leonardo", 40,35,RED,WHITE);
-   clcd.setStr("Robotics", 56,35,RED,WHITE);
-   //delay(2500);
-   //lcd.clear();
-   lcd.setCursor(4,0);
-   lcd.print("Leonardo");
-   lcd.setCursor(4,1);
-   lcd.print("Robotics");
-   
+  //clcd.setStr("Security System", 2,6,RED,WHITE);
+  //clcd.setStr("Debugging", 100,30 ,RED,WHITE);
+  //clcd.setStr("Leonardo", 40,35,RED,WHITE);
+  //clcd.setStr("Robotics", 56,35,RED,WHITE);
+  //delay(2500);
+  //lcd.clear();
+  //lcd.setCursor(4,0);
+  //lcd.print("Leonardo");
+  //lcd.setCursor(4,1);
+  //lcd.print("Robotics");
+
+
+  if(t==second())
+  {
+    digitalClockDisplay( );
+    t++;
+  }
+  if(t==60)t=0;
 }
 
+void printDigits(byte digits){
+  // utility function for digital clock display: prints preceding colon and leading 0
+  lcd.print(":");
+  if(digits < 10)
+    lcd.print('0');
+  lcd.print(digits,DEC);
+}
+void digitalClockDisplay(){
+  lcd.clear();
+  lcd.setCursor(3,0);
 
+  //lcd.print(DateTimeStrings.dayStr(DateTime.DayofWeek));
+  if(day() <10)
+    lcd.print('0');
+  lcd.print(day(),DEC);
+  lcd.print(".");
+
+  //lcd.print(DateTimeStrings.monthStr(DateTime.Month));
+  if(month() <10)
+    lcd.print('0');
+  lcd.print(month(),DEC);
+  lcd.print(".");
+  lcd.print(year(),DEC);
+
+  //lcd.print(" ");
+  lcd.setCursor(4,1);
+  if(hour() <10)
+    lcd.print('0');
+
+  // digital clock display of current time
+  lcd.print(hour(),DEC);
+  printDigits(minute());
+  printDigits(second());
+}
